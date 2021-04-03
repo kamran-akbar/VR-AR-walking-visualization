75058c7d4efb8172a99837c3deed7382c3b6d613
Ville <villeuhlgren@gmail.com>
2021-04-03 13:11:30 +0300

    First add, later on new rules for adding

---
+++ Assets/MouseLook.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+    float xRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update, there is a also a new way to use input system package, not in use now.
+    // Use Time.deltaTime, it adds to the update frames
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);//Unity and vscode wants to write this on caps, wont work
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
