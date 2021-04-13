da16fc5c2c94d30946d8eccd5da1c6e7a58aa15f
Kamran Akbar (TAU) <kamran.akbar@tuni.fi>
2021-04-13 20:39:12 +0300

    Building's name will be shown by looking at them

---
+++ Assets/Codes/BuildingRayCast.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingRayCast : MonoBehaviour
+{
+    public Transform camera;
+
+    Transform prevBuilding = null;
+
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, camera.forward, out hit, Mathf.Infinity))
+        {
+            Transform currentBuilding = hit.collider.gameObject.transform;
+            if (hit.collider.gameObject.transform.tag == "Interactable")
+            {
+                if (prevBuilding == null)
+                {
+                    Debug.Log(currentBuilding.gameObject.name);
+                    currentBuilding.GetChild(0).GetChild(0).gameObject.active = true;
+                    prevBuilding = currentBuilding;
+                }
+                else if (prevBuilding != currentBuilding)
+                {
+                    prevBuilding.GetChild(0).GetChild(0).gameObject.active = false;
+                    currentBuilding.GetChild(0).GetChild(0).gameObject.active = true;
+                    prevBuilding = currentBuilding;
+                }
+                Debug.Log(hit.collider.gameObject.transform.name);
+            }
+        }
+                
+    }
+}
---
+++ Assets/Codes/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller; //controller for the move vector3
+    public float speed = 12f; //movement speed for the player/camera
+    Animator animator;
+    //public Character controller;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        if (Mathf.Abs(z) > 0.01f)
+        {
+            animator.SetBool("Move", true);
+        }
+        else
+        {
+            animator.SetBool("Move", false);
+        }
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
